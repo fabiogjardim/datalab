c218d061202a8a01f1e19ad9ca2fd82632260190
Fábio Jardim <fabiogjardim@hotmail.com>
2024-08-07 16:46:43 -0300

    Create script.bat

---
+++ script.bat
@@ -0,0 +1,7 @@
+mkdir .\postgres\volume\pg_notify
+mkdir .\postgres\volume\pg_tblspc
+mkdir .\postgres\volume\pg_twophase
+mkdir .\postgres\volume\pg_commit_ts
+mkdir .\postgres\volume\pg_snapshots
+mkdir .\postgres\volume\pg_stat_tmp
+mkdir .\postgres\volume\pg_logical\mappings
